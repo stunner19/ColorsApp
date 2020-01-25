7bc6431fb4cc79a9c7c2dd787ab37926d8bd3695
Karanpal Singh <singhkaranpal81@gmail.com>
2020-01-25 16:43:29 +0530

    Adding Link to ColorBox

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component{
                         />
                     )}
                 />
+                <Route exact path = '/palette/:paletteId/:colorId' />
             </Switch>
         );
     }
