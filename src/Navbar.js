5a1895975831e3a89abd46fde4e48876ad8f56f1
Karanpal Singh <singhkaranpal81@gmail.com>
2020-01-26 18:38:56 +0530

    Adding styles to all components using JSs

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,9 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
+import { withStyles } from '@material-ui/styles';
 import Slider from 'rc-slider';
+import styles from './styles/NavbarStyles';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 import { IconButton } from '@material-ui/core';
@@ -36,22 +38,22 @@ class Nabvar extends Component {
     }
 
     render(){
-        const { level, changeLevel, showingAllColors } = this.props;
+        const { level, changeLevel, showingAllColors, classes } = this.props;
         const { format, open } = this.state;
         return(
-            <header className = "Navbar">
-                <div className = "logo">
+            <header className = {classes.Navbar}>
+                <div className = {classes.logo}>
                     <Link to = "/">reactcolorpicker</Link>
                 </div>
                 {showingAllColors && (
-                    <div className = "slider-container">
+                    <div>
                         <span>Level : {level} </span>
-                        <div className = "slider">
+                        <div className = {classes.slider}>
                             <Slider defaultValue = {level} min = {100} max = {900} step = {100} onAfterChange = {changeLevel} />                     
                         </div>  
                     </div> 
                 )}
-                <div className = "select-container">
+                <div className = {classes.selectContainer}>
                     <Select value = {format} onChange = {this.handleFormatChange}>
                         <MenuItem value = "hex">HEX - #ffffff</MenuItem>
                         <MenuItem value = "rgb">RGB - rgb(255,255,255)</MenuItem>
@@ -78,4 +80,4 @@ class Nabvar extends Component {
     }
 }
 
-export default Nabvar;
+export default withStyles(styles)(Nabvar);
