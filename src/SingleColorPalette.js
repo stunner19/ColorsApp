82816a23955cfefa5b54f59ae6dee9ad8c8d0bd5
Karanpal Singh <singhkaranpal81@gmail.com>
2020-01-26 17:45:02 +0530

    Styling all components using JSS

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,52 +1,10 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import './Palette.css';
 import Nabvar from './Navbar';
 import PaletteFooter from './PaletteFooter';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
-import chroma from 'chroma-js';
-
-const styles = {
-    Palette : {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-    colors : {
-        height: "90%"
-    },
-    goBack : {
-        width: "20%",
-        height: (props) => (props.showingFullPalette ? "25%" : "50%"),
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-4.4px",
-        opacity : 1,
-        backgroundColor : "black",
-        "& a" : {
-            color : "white",
-            width: "100px",
-            height: "30px",   
-            position: "absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            marginLeft: "-50px",
-            marginTop: "-15px",
-            textAlign: "center",
-            outline: "none",
-            background: "rgba(255, 255, 255, 0.3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            textTransform: "uppercase",
-            border: "none",
-            textDecoration: "none",
-        }
-    }
-};
+import styles from './styles/PaletteStyles';
 
 class SingleColorPalette extends Component {
     constructor(props){
