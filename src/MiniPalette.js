4060577e6a4c5dea4175aa400612c0ab47875f3b
Karanpal Singh <singhkaranpal81@gmail.com>
2020-02-01 13:54:27 +0530

    Adding Pure Component to MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/MiniPaletteStyles';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-class MiniPalette extends Component{
+class MiniPalette extends PureComponent{
     constructor(props){
         super(props);
         this.deletePalette = this.deletePalette.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     deletePalette(e){
         e.stopPropagation();
         this.props.openDialog(this.props.id);
+    }
+
+    handleClick(){
+        this.props.goToPalette(this.props.id);
     }
 
     render(){
@@ -21,7 +26,7 @@ class MiniPalette extends Component{
         ));
     
         return(
-            <div className = {classes.root} onClick = {handleClick}>
+            <div className = {classes.root} onClick = {this.handleClick}>
                 <DeleteIcon className = {classes.deleteIcon} style = {{ transition : "all 0.3s ease-in-out"}} onClick = {this.deletePalette} />
                 <div className = {classes.colors}>
                     {miniColorBoxes}
