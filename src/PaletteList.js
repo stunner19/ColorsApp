11fc98e03ed0116b26d6cdba4ced2c67aa2437d4
Karanpal Singh <singhkaranpal81@gmail.com>
2020-02-01 11:50:10 +0530

    Adding Fade transition to PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import MiniPalette from './MiniPalette';
 import styles from './styles/PaletteListStyles';
 
@@ -22,17 +23,20 @@ class PaletteList extends Component {
                         <h1 className = {classes.heading}>React Colors</h1>
                         <Link to = '/palette/new'>Create Palette</Link>
                     </nav>
-                    <div className = {classes.palettes}>
+                    {/* see video 3 part 7 */}
+                    <TransitionGroup className = {classes.palettes}>
                         {palettes.map(palette => (
-                            <MiniPalette 
-                                {...palette} 
-                                handleClick = {() => this.goToPalette(palette.id)} 
-                                handleDelete = {deletePalette}
-                                key = {palette.id}
-                                id = {palette.id}
-                            />
+                            <CSSTransition key = {palette.id} classNames = 'fade' timeout = {500}>
+                                <MiniPalette 
+                                    {...palette} 
+                                    handleClick = {() => this.goToPalette(palette.id)} 
+                                    handleDelete = {deletePalette}
+                                    key = {palette.id}
+                                    id = {palette.id}
+                                />
+                            </CSSTransition>
                         ))}
-                    </div>
+                    </TransitionGroup>
                 </div>
             </div>
         );
