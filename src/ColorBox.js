e26d1a0865be581208e355e1ff222bb8d93f5595
Karanpal Singh <singhkaranpal81@gmail.com>
2020-02-01 14:02:28 +0530

    Adding classNames to ColorBox and fix Navbar slider

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import  { CopyToClipboard } from 'react-copy-to-clipboard';
 import { withStyles } from '@material-ui/styles';
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import styles from './styles/ColorBoxStyles';
 
@@ -33,8 +34,13 @@ class ColorBox extends Component {
         return(
             <CopyToClipboard text = {background} onCopy = {this.changeCopyState}>
                 <div style = {{ background : background }} className = {classes.ColorBox}>
-                    <div style = {{background : background}} className = {`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
-                    <div className = {`${classes.copyMessage} ${copied && classes.showMessage}`}>
+                    <div style = {{background : background}} className = {classNames(classes.copyOverlay, {
+                        [classes.showOverlay] : copied 
+                        })} 
+                    />
+                    <div className = {classNames(classes.copyMessage, {
+                        [classes.showMessage] : copied
+                    })}>
                         <h1 className = {classes.copyText}>copied!</h1>
                         <p className = {classes.copyText}>{background}</p>
                     </div>
